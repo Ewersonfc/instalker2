a9491eabfe2f4c2431ab78259c85bdee345fd152
Ewerson <ewersonfc@gmail.com>
2021-04-23 22:12:48 -0300

    api

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import firebase from "firebase";
 import {
   Page,
   Navbar,
@@ -17,41 +16,6 @@ import {
   Col,
   Button
 } from 'framework7-react';
-
-const login = () => {
-
-  
-  var provider = new firebase.auth.FacebookAuthProvider();
-
-firebase
-  .auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-    /** @type {firebase.auth.OAuthCredential} */
-    var credential = result.credential;
-
-    // The signed-in user info.
-    var user = result.user;
-
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var accessToken = credential.accessToken;
-
-    // ...
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-
-    // ...
-  });
-
-  firebase.auth().signInWithRedirect(provider);
-}
 
 
 const HomePage = () => (
@@ -126,4 +90,4 @@ const HomePage = () => (
     </List>
   </Page>
 );
-export default login;
+export default HomePage;
